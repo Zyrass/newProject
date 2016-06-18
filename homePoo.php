591ec026d304678cf4493577391480c68772a9dd
Alain Guillon <Alain Guillon>
2016-06-18 10:54:03 +0200

    Mise en page de ma homePoo.php fini à 50%

--- homePoo.php
+++ homePoo.php
@@ -37,15 +37,85 @@
   </head>
   <body>
     <main>
+      <!-- =================================================================================================== -->
+      <!--                                            HEADER                                                   -->
+      <!-- =================================================================================================== -->
       <header>
-
+        <div class="container-fluid"> <!-- start container-fluid dans le header -->
+          <div class="row"> <!-- start de la row du container-fluid dans le header -->
+            <div class="col-xs-12"> <!-- start du col-xs-12 de la row issu du container-fluid dans le header -->
+                <hgroup class="well">
+                  <h1 class="text-capitalize text-info text-center">NewProject !!</h1>
+                  <h2 class="text-center"><small>Les liens qui suivront permettent de tester plusieurs méthodes en fonction d'un contexte bien précis. Tous seras expliqués</small></h2>
+                </hgroup>
+            </div> <!-- end col-xs-12 du container-fluid dans le header -->
+          </div> <!-- end de la row du container-fluid dans le header -->
+        </div> <!-- end du container-fluid dans le header -->
       </header>
+      <!-- =================================================================================================== -->
+      <!--                                           SECTION                                                   -->
+      <!-- =================================================================================================== -->
+      <section>
+        <div class="container"> <!-- start container dans la section -->
+          <div class="row"> <!-- start de la row du container dans la section -->
+            <div class="col-xs-12 jumbotron"> <!-- start du col-xs-12 de la row issu du container dans la section -->
+              <legend>Présentation</legend>
+              <blockquote>
+                Je vous propose ici 6 articles traitant sur des méthodes établie en POO !
+              </blockquote>
+            </div> <!-- end col-xs-12 du container dans la section -->
+          </div> <!-- end de la row du container dans la section -->
+          <div class="row row2">
+            <div class="col-xs-12 col-md-4">
+              <article class="well">
+                <legend class="text-right text-warning">appMovie</legend>
+              </article>
+            </div>
+            <div class="col-xs-12 col-md-4">
+              <article class="well">
+                <legend class="text-right text-warning">appPersonnelActors</legend>
+              </article>
+            </div>
+            <div class="col-xs-12 col-md-4">
+              <article class="well">
+                <legend class="text-right text-warning">appPersonnelActorsFilm</legend>
+              </article>
+            </div>
+            <div class="col-xs-12 col-md-4">
+              <article class="well">
+                <legend class="text-right text-warning">appPersonnelActorsSerie</legend>
+              </article>
+            </div>
+            <div class="col-xs-12 col-md-4">
+              <article class="well">
+                <legend class="text-right text-warning">appPersonnelDirector</legend>
+              </article>
+            </div>
+            <div class="col-xs-12 col-md-4">
+              <article class="well">
+                <legend class="text-right text-warning">appSupportMovie</legend>
+              </article>
+            </div>
+        </div> <!-- end du container dans la section -->
+      </section>
     </main>
+    <!-- =================================================================================================== -->
+    <!--                                            FOOTER                                                   -->
+    <!-- =================================================================================================== -->
     <footer>
-
+      <div class="container-fluid"> <!-- start container-fluid dans le footer -->
+        <div class="row"> <!-- start de la row du container-fluid dans le footer -->
+          <div class="col-xs-12"> <!-- start du col-xs-12 de la row issu du container-fluid dans le footer -->
+            <div class="well">
+              <h4 class="text-center text-capitalize text-info">Quelques questions pour éveiller notre curiosité !</h4>
+              <p class="text-center">
+                <a href="utileCuriosite.php">Cliquez vite sur moi afin d'accéder à la page traitant sur des questions intéréssantes </a>
+              </p>
+            </div>
+          </div> <!-- end col-xs-12 de la row issu du container-fluid dans le footer -->
+        </div> <!-- end de la row du container-fluid dans le footer -->
+      </div> <!-- end du container-fluid dans le footer -->
     </footer>
-
-
     <!-- =================================================================================================== -->
     <!-- /////////////////////////////////////////////////////////////////////////////////////////////////// -->
     <!-- =================================================================================================== -->
@@ -56,5 +126,8 @@
     <!--                                        SCRIPT BOOTSTRAP                                             -->
     <!-- =================================================================================================== -->
     <script src="style/bootstrap/js/bootstrap.min.js"></script>
+    <!-- =================================================================================================== -->
+    <!-- /////////////////////////////////////////////////////////////////////////////////////////////////// -->
+    <!-- =================================================================================================== -->
   </body>
 </html>
