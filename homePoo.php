6b5d6084a92786e2cd5d30ffb8e72457ae07491b
Alain Guillon <Alain Guillon>
2016-06-19 09:29:26 +0200

    Modif de l'interface avec quelques liens

--- homePoo.php
+++ homePoo.php
@@ -35,7 +35,7 @@
     <!-- /////////////////////////////////////////////////////////////////////////////////////////////////// -->
     <!-- =================================================================================================== -->
   </head>
-  <body>
+  <body class="bg-danger">
     <main>
       <!-- =================================================================================================== -->
       <!--                                            HEADER                                                   -->
@@ -58,7 +58,7 @@
       <section>
         <div class="container"> <!-- start container dans la section -->
           <div class="row"> <!-- start de la row du container dans la section -->
-            <div class="col-xs-12 jumbotron"> <!-- start du col-xs-12 de la row issu du container dans la section -->
+            <div class="col-xs-12 well"> <!-- start du col-xs-12 de la row issu du container dans la section -->
               <legend>Présentation</legend>
               <blockquote>
                 Je vous propose ici 6 articles traitant sur des méthodes établie en POO !
@@ -66,36 +66,73 @@
             </div> <!-- end col-xs-12 du container dans la section -->
           </div> <!-- end de la row du container dans la section -->
           <div class="row row2">
-            <div class="col-xs-12 col-md-4">
+            <div class="col-xs-12 col-lg-offset-2 col-lg-8">
               <article class="well">
                 <legend class="text-right text-warning">appMovie</legend>
+                  <p>Appli sur les films<br />
+                    <a href='src/class/movies/ClassMovie.php'>Clique ici...</a>
+                  </p>
+                  <footer class="pull-right">src/class/movies/<strong><span class="text-success">ClassMovie.php</span></strong></footer><br />
               </article>
             </div>
-            <div class="col-xs-12 col-md-4">
-              <article class="well">
-                <legend class="text-right text-warning">appPersonnelActors</legend>
-              </article>
-            </div>
-            <div class="col-xs-12 col-md-4">
-              <article class="well">
-                <legend class="text-right text-warning">appPersonnelActorsFilm</legend>
-              </article>
-            </div>
-            <div class="col-xs-12 col-md-4">
-              <article class="well">
-                <legend class="text-right text-warning">appPersonnelActorsSerie</legend>
-              </article>
-            </div>
-            <div class="col-xs-12 col-md-4">
-              <article class="well">
-                <legend class="text-right text-warning">appPersonnelDirector</legend>
-              </article>
-            </div>
-            <div class="col-xs-12 col-md-4">
+            <!-- =================================================================================================== -->
+            <div class="col-xs-12">
               <article class="well">
                 <legend class="text-right text-warning">appSupportMovie</legend>
+                  <p>Appli sur les le supports des films<br />
+                    <a href='src/class/movies/supports/ClassBlueRay.php'>BlueRay</a> -
+                    <a href='src/class/movies/supports/ClassDvd.php'>Dvd</a> -
+                    <a href='src/class/movies/supports/ClassDvdRom.php'>DvdRom</a>
+                  </p>
+                  <footer>
+                    src/class/movies/supports/<br />
+                      { <strong><span class="text-success">ClassBlueRay.php</span></strong> |
+                      <strong><span class="text-success">ClassDvd.php</span></strong> |
+                      <strong><span class="text-success">ClassDvdRom.php</span></strong> }
+                  </footer><br />
               </article>
             </div>
+            <!-- =================================================================================================== -->
+            <div class="col-xs-12 col-lg-offset-2 col-lg-8">
+              <article class="well">
+                <legend class="text-right text-warning">appPersonnelActors</legend>
+                  <p>Appli sur les acteurs<br />
+                    <a href='src/class/personnels/actors/ClassActors.php'>Clique ici...</a>
+                  </p>
+                  <footer class="pull-right">src/class/personnels/actors/<strong><span class="text-success">ClassActors.php</span></strong></footer><br />
+              </article>
+            </div>
+            <!-- =================================================================================================== -->
+            <div class="col-xs-12 col-lg-6">
+              <article class="well">
+                <legend class="text-right text-warning">appPersonnelActorsFilm</legend>
+                  <p>Appli sur les acteurs qui jouent dans des films<br />
+                    <a href='src/class/personnels/actors/roles/ClassActorsFilm.php'>Clique ici...</a>
+                  </p>
+                  <footer class="pull-right">src/class/personnels/actors/roles/<strong><span class="text-success">ClassActorsFilm.php</span></strong></footer><br />
+              </article>
+            </div>
+            <!-- =================================================================================================== -->
+            <div class="col-xs-12 col-lg-6">
+              <article class="well">
+                <legend class="text-right text-warning">appPersonnelActorsSerie</legend>
+                  <p>Appli sur les acteurs qui jouent dans des séries<br />
+                    <a href='src/class/personnels/actors/roles/ClassActorsSerie.php'>Clique ici...</a>
+                  </p>
+                  <footer class="pull-right">src/class/personnels/actors/roles/<strong><span class="text-success">ClassActorsSerie.php</span></strong></footer><br />
+              </article>
+            </div>
+            <!-- =================================================================================================== -->
+            <div class="col-xs-12 col-lg-offset-2 col-lg-8">
+              <article class="well">
+                <legend class="text-right text-warning">appPersonnelDirector</legend>
+                  <p>Appli sur les Réalisateurs
+                    <a href='src/class/personnels/directors/ClassDirectors.php'>Clique ici...</a>
+                  </p>
+                  <footer class="pull-right">src/class/personnels/directors/<strong><span class="text-success">ClassDirectors.php</span></strong></footer><br />
+              </article>
+            </div>
+            <!-- =================================================================================================== -->
         </div> <!-- end du container dans la section -->
       </section>
     </main>
